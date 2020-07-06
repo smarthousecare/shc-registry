a03cbc375f3c251ba5c7ec57d89cd41dacd00414
Ernani Ramos <ernaniramos@gmail.com>
2020-07-06 09:51:12 +0100

    OAuth with spring security 5

--- src/main/java/com/house/care/registryserver/config/OAuth2SsoConfiguration.java
+++ src/main/java/com/house/care/registryserver/config/OAuth2SsoConfiguration.java
@@ -1,61 +1,103 @@
 package com.house.care.registryserver.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.house.care.registryserver.config.security.oauth2.AuthoritiesConstants;
 
-@EnableOAuth2Sso
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final RequestMatcher authorizationHeaderRequestMatcher;
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(
+            SecurityProperties properties,
+            ObjectProvider<PasswordEncoder> passwordEncoder) {
+        SecurityProperties.User user = properties.getUser();
+        List<String> roles = user.getRoles();
+        return new InMemoryUserDetailsManager(User.withUsername(user.getName())
+                .password(getOrDeducePassword(user, passwordEncoder.getIfAvailable()))
+                .roles(StringUtils.toStringArray(roles)).build());
+    }
 
-    public OAuth2SsoConfiguration(@Qualifier("authorizationHeaderRequestMatcher") RequestMatcher authorizationHeaderRequestMatcher) {
-
-        this.authorizationHeaderRequestMatcher = authorizationHeaderRequestMatcher;
+    private String getOrDeducePassword(SecurityProperties.User user,
+                                       PasswordEncoder encoder) {
+        if (encoder != null) {
+            return user.getPassword();
+        }
+        return "{noop}" + user.getPassword();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .csrf().disable()
-                .requestMatcher(new NegatedRequestMatcher(authorizationHeaderRequestMatcher))
-                .httpBasic()
-                .realmName("Registry Server")
-                .and()
-                .authorizeRequests()
-                .antMatchers("/services/**").authenticated()
-                .antMatchers("/eureka/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/api/profile-info").permitAll()
-                .antMatchers("/api/**").authenticated()
-                .antMatchers("/config/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/management/health").permitAll()
-                .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/registry/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .anyRequest().authenticated().and()
-                .oauth2Login()
-                .and()
-                .oauth2ResourceServer().jwt();
+        .cors()
+        .and()
+        .csrf()
+        .disable()
+        .headers()
+        .frameOptions()
+        .disable()
+        .and()
+        .httpBasic()
+        .realmName("Registry Server")
+        .and()
+        .authorizeRequests()
+        .antMatchers("/services/**").authenticated()
+        .antMatchers("/eureka/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .antMatchers("/api/profile-info").permitAll()
+        .antMatchers("/api/**").authenticated()
+        .antMatchers("/config/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .antMatchers("/management/health").permitAll()
+        .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .antMatchers("/registry/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .anyRequest().authenticated().and()
+        .oauth2Login()
+        .and()
+        .oauth2ResourceServer().jwt();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/app/**/*.{js,html}")
+                .antMatchers("/swagger-ui/**")
+                .antMatchers("/content/**");
     }
 
     @Bean
-    public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
 
-        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(filter);
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
-        return registration;
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
-
 }
--- src/main/java/com/house/care/registryserver/config/security/oauth2/AuthoritiesConstants.java
+++ src/main/java/com/house/care/registryserver/config/security/oauth2/AuthoritiesConstants.java
@@ -5,7 +5,7 @@ package com.house.care.registryserver.config.security.oauth2;
  */
 public final class AuthoritiesConstants {
 
-    public static final String ADMIN = "ADMIN";
+    public static final String ADMIN = "ROLE_ADMIN";
 
     private AuthoritiesConstants() {
 
