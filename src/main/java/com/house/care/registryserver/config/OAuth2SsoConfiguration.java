02c1617169064bf133ae52b7712a365403ae424c
Ernani Ramos <ernaniramos@gmail.com>
2020-07-22 11:12:48 +0100

    Implement config server and authentication rules

--- src/main/java/com/house/care/registryserver/config/OAuth2SsoConfiguration.java
+++ src/main/java/com/house/care/registryserver/config/OAuth2SsoConfiguration.java
@@ -1,9 +1,12 @@
 package com.house.care.registryserver.config;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +17,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
@@ -29,10 +37,14 @@ import com.house.care.registryserver.config.security.oauth2.AuthoritiesConstants
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.security.oauth2.client.provider.okta.rolesClaim}")
+    private String rolesClaim;
+
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(
             SecurityProperties properties,
             ObjectProvider<PasswordEncoder> passwordEncoder) {
+
         SecurityProperties.User user = properties.getUser();
         List<String> roles = user.getRoles();
         return new InMemoryUserDetailsManager(User.withUsername(user.getName())
@@ -41,7 +53,8 @@ public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     private String getOrDeducePassword(SecurityProperties.User user,
-                                       PasswordEncoder encoder) {
+            PasswordEncoder encoder) {
+
         if (encoder != null) {
             return user.getPassword();
         }
@@ -52,34 +65,60 @@ public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-        .cors()
-        .and()
-        .csrf()
-        .disable()
-        .headers()
-        .frameOptions()
-        .disable()
-        .and()
-        .httpBasic()
-        .realmName("Registry Server")
-        .and()
-        .authorizeRequests()
-        .antMatchers("/services/**").authenticated()
-        .antMatchers("/eureka/**").hasAuthority(AuthoritiesConstants.ADMIN)
-        .antMatchers("/api/profile-info").permitAll()
-        .antMatchers("/api/**").authenticated()
-        .antMatchers("/config/**").hasAuthority(AuthoritiesConstants.ADMIN)
-        .antMatchers("/management/health").permitAll()
-        .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
-        .antMatchers("/registry/**").hasAuthority(AuthoritiesConstants.ADMIN)
-        .anyRequest().authenticated().and()
-        .oauth2Login()
-        .and()
-        .oauth2ResourceServer().jwt();
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .httpBasic()
+                .realmName("Registry Server")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/services/**").authenticated()
+                .antMatchers("/eureka/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/api/profile-info").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/config/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/management/health").permitAll()
+                .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/registry/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login().userInfoEndpoint()
+                .userAuthoritiesMapper(this.userAuthoritiesMapper())
+                .and()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+    }
+
+    private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach(authority -> {
+                if (authority instanceof OidcUserAuthority) {
+                    OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
+                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+
+                    List<Object> groups = userInfo.getClaim(rolesClaim);
+                    groups.forEach(group -> mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_".concat(String.valueOf(group)))));
+                }
+            });
+
+            return mappedAuthorities;
+        };
     }
 
     @Override
     public void configure(WebSecurity web) {
+
         web.ignoring()
                 .antMatchers("/app/**/*.{js,html}")
                 .antMatchers("/swagger-ui/**")
--- src/test/java/com/house/care/registryserver/RegistryServerApplicationTests.java
+++ src/test/java/com/house/care/registryserver/RegistryServerApplicationTests.java
@@ -1,19 +1,66 @@
 package com.house.care.registryserver;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RegistryServerApplicationTests {
+
+    private final static String TEST_USER_ID = "user-id-123";
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     @DisplayName("Test Spring @Autowired Integration")
     void contextLoads() {
 
         assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("Test Spring Health")
+    void testHealth() throws Exception {
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/management/health")
+                .with(user(TEST_USER_ID))
+                .with(csrf())
+                // .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andReturn();
+        String resultCZ = result.getResponse().getContentAsString();
+        assertNotNull(resultCZ);
+    }
+
+    @Test
+    @DisplayName("Test Spring Info")
+    // @WithMockUser(roles = "ADMIN")
+    void testEureka() throws Exception {
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/management/info")
+                .with(user(TEST_USER_ID).roles("ADMIN"))
+                .with(csrf())
+                // .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andReturn();
+        String resultCZ = result.getResponse().getContentAsString();
+        assertNotNull(resultCZ);
     }
 
 }
